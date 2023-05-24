a0d6e1050017ffed4ed26e8059609065384a995d
Adam <adam.shad@mail.ru>
2023-05-24 14:31:42 +0300

    git init

---
+++ src/components/1pages/DHome.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DProgressBar from '@/components/2widgets/DProgressBar.vue';
+</script>
+
+<template>
+  <main class="d-home">
+    <div class="container">
+      <DProgressBar />
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import DHome from '@/components/1pages/DHome.vue';
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: DHome,
+    },
+  ],
+});
+
+export default router;
